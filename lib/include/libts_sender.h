858051b81e999c78dd99650c7ec28b1f98276a1d
Lujiang <lujiang0111@gmail.com>
2024-02-23 17:33:24 +0800

    添加ITsSender接口

--- lib/include/libts_sender.h
+++ lib/include/libts_sender.h
@@ -1,6 +1,9 @@
 ﻿#ifndef LIBTS_SENDER_INCLUDE_LIBTS_SENDER_H_
 #define LIBTS_SENDER_INCLUDE_LIBTS_SENDER_H_
 
+#include <cstdint>
+#include <memory>
+
 #if defined(_WIN32)
 #if defined(LIBTS_SENDER_API_EXPORT)
 #define LIBTS_SENDER_API __declspec(dllexport)
@@ -10,5 +13,27 @@
 #else
 #define LIBTS_SENDER_API
 #endif
+
+namespace ltss
+{
+
+using NodeId = int64_t;
+
+class ITsSender
+{
+public:
+    virtual ~ITsSender() = default;
+
+    virtual NodeId CreateNode(const char *param_str) = 0;
+    virtual void DestroyNode(NodeId node_id) = 0;
+
+    virtual bool StartNode(NodeId node_id) = 0;
+    virtual void StopNode(NodeId node_id) = 0;
+
+};
+
+LIBTS_SENDER_API std::shared_ptr<ITsSender> CreateTsSender();
+
+}
 
 #endif // !LIBTS_SENDER_INCLUDE_LIBTS_SENDER_H_
