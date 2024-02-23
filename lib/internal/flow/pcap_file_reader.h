649fe757d645a4c489bfd5defc87212360ae3ef5
Lujiang <lujiang0111@gmail.com>
2024-02-23 17:50:56 +0800

    添加一些定义

---
+++ lib/internal/flow/pcap_file_reader.h
@@ -0,0 +1,30 @@
+﻿#ifndef LIBTS_SENDER_INTERNAL_FLOW_PCAP_FILE_READER_H_
+#define LIBTS_SENDER_INTERNAL_FLOW_PCAP_FILE_READER_H_
+
+#include <string>
+#include "libflow.h"
+
+namespace ltss
+{
+namespace flow
+{
+
+class PcapFileReaderFlow : public lflow::IFlow
+{
+public:
+    explicit PcapFileReaderFlow(const std::string &flow_id);
+    virtual ~PcapFileReaderFlow();
+
+    virtual bool Init(const char *param_str, void *opaque);
+    virtual void DeInit();
+    virtual bool Modify(const char *param_str, void *opaque);
+
+    virtual std::shared_ptr<lflow::IPacketList> Process(std::shared_ptr<lflow::IPacketList> in_packet_list);
+    virtual bool Control(const char *type, const char *param_str, void *opaque);
+
+};
+
+}
+}
+
+#endif // !LIBTS_SENDER_INTERNAL_FLOW_PCAP_FILE_READER_H_
---
+++ lib/internal/ts_sender.h
@@ -0,0 +1,11 @@
+﻿#ifndef LIBTS_SENDER_INTERNAL_TS_SENDER_H_
+#define LIBTS_SENDER_INTERNAL_TS_SENDER_H_
+
+#include "libts_sender.h"
+
+namespace ltss
+{
+
+}
+
+#endif // !LIBTS_SENDER_INTERNAL_TS_SENDER_H_
