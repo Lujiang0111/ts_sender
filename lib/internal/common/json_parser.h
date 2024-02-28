d806a9069c52361523f25f11453a85b8d878f703
Lujiang <lujiang0111@gmail.com>
2024-02-28 17:58:01 +0800

    更新接口

--- lib/internal/common/json_parser.h
+++ lib/internal/common/json_parser.h
@@ -1,6 +1,7 @@
 ﻿#ifndef LIBTS_SENDER_INTERNAL_COMMON_JSON_PARSER_H_
 #define LIBTS_SENDER_INTERNAL_COMMON_JSON_PARSER_H_
 
+#include <cstdint>
 #include <string>
 #include "rapidjson/document.h"
 
@@ -8,7 +9,21 @@ bool ParseJsonToString(const rapidjson::Document &json_doc, std::string &str);
 bool ParseJsonToString(const rapidjson::Value &json_val, std::string &str);
 bool ParseStringToJson(rapidjson::Document &json_doc, const std::string &str);
 
-bool SetJsonChildInt(rapidjson::Document &json_doc, const std::string &key, int val);
-bool SetJsonChildInt(rapidjson::Value &json_val, rapidjson::Document::AllocatorType &allocator, const std::string &key, int val);
+bool IsJsonChildValid(const rapidjson::Document &json_doc, const std::string &key, rapidjson::Type type);
+bool IsJsonChildValid(const rapidjson::Value &json_val, const std::string &key, rapidjson::Type type);
+
+bool GetJsonChildNumber(const rapidjson::Document &json_doc, const std::string &key, int64_t &val);
+bool GetJsonChildNumber(const rapidjson::Value &json_val, const std::string &key, int64_t &val);
+bool GetJsonChildBool(const rapidjson::Document &json_doc, const std::string &key, bool &val);
+bool GetJsonChildBool(const rapidjson::Value &json_val, const std::string &key, bool &val);
+bool GetJsonChildString(const rapidjson::Document &json_doc, const std::string &key, std::string &val);
+bool GetJsonChildString(const rapidjson::Value &json_val, const std::string &key, std::string &val);
+
+void SetJsonChildNumber(rapidjson::Document &json_doc, const std::string &key, int64_t val);
+void SetJsonChildNumber(rapidjson::Value &json_val, rapidjson::Document::AllocatorType &allocator, const std::string &key, int64_t val);
+void SetJsonChildBool(rapidjson::Document &json_doc, const std::string &key, bool val);
+void SetJsonChildBool(rapidjson::Value &json_val, rapidjson::Document::AllocatorType &allocator, const std::string &key, bool val);
+void SetJsonChildString(rapidjson::Document &json_doc, const std::string &key, const std::string &val);
+void SetJsonChildString(rapidjson::Value &json_val, rapidjson::Document::AllocatorType &allocator, const std::string &key, const std::string &val);
 
 #endif // !LIBTS_SENDER_INTERNAL_COMMON_JSON_PARSER_H_
--- lib/src/common/json_parser.cpp
+++ lib/src/common/json_parser.cpp
@@ -32,7 +32,51 @@ bool ParseStringToJson(rapidjson::Document &json_doc, const std::string &str)
     return true;
 }
 
-bool SetJsonChildInt(rapidjson::Document &json_doc, const std::string &key, int val)
+bool IsJsonChildValid(const rapidjson::Document &json_doc, const std::string &key, rapidjson::Type type)
+{
+    if ((!json_doc.IsObject()) || (!json_doc.HasMember(key.c_str())) || (json_doc[key.c_str()].IsNull()))
+    {
+        return false;
+    }
+
+    const rapidjson::Value &json_val = json_doc[key.c_str()];
+    return (json_val.GetType() == type);
+}
+
+bool IsJsonChildValid(const rapidjson::Value &json_val, const std::string &key, rapidjson::Type type)
+{
+    if ((!json_val.IsObject()) || (!json_val.HasMember(key.c_str())) || (json_val[key.c_str()].IsNull()))
+    {
+        return false;
+    }
+
+    const rapidjson::Value &json_child_val = json_val[key.c_str()];
+    return (json_val.GetType() == type);
+}
+
+bool GetJsonChildNumber(const rapidjson::Document &json_doc, const std::string &key, int64_t &val)
+{
+    if (!IsJsonChildValid(json_doc, key, rapidjson::kNumberType))
+    {
+        return false;
+    }
+
+    val = json_doc[key.c_str()].GetInt();
+    return true;
+}
+
+bool GetJsonChildNumber(const rapidjson::Value &json_val, const std::string &key, int64_t &val)
+{
+    if (!IsJsonChildValid(json_val, key, rapidjson::kNumberType))
+    {
+        return false;
+    }
+
+    val = json_val[key.c_str()].GetInt();
+    return true;
+}
+
+void SetJsonChildNumber(rapidjson::Document &json_doc, const std::string &key, int64_t val)
 {
     auto &&allocator = json_doc.GetAllocator();
     json_doc.AddMember(rapidjson::Value(key.c_str(), allocator).Move(),
@@ -40,7 +84,7 @@ bool SetJsonChildInt(rapidjson::Document &json_doc, const std::string &key, int 
         allocator);
 }
 
-bool SetJsonChildInt(rapidjson::Value &json_val, rapidjson::Document::AllocatorType &allocator, const std::string &key, int val)
+void SetJsonChildNumber(rapidjson::Value &json_val, rapidjson::Document::AllocatorType &allocator, const std::string &key, int64_t val)
 {
     json_val.AddMember(rapidjson::Value(key.c_str(), allocator).Move(),
         rapidjson::Value(val).Move(),
